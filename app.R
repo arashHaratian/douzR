6cb2ffc5dab6ddf3358f676c87f59e658857566f
Arash <45104106+arashHaratian@users.noreply.github.com>
2021-05-07 22:56:52 +0430

    handle end game

--- app.R
+++ app.R
@@ -24,12 +24,13 @@ server <- function(input, output, session) {
   eps <- 0.01
   alpha <- 0.5
   old_state_index <- reactiveVal()
+  winner <- reactiveVal()
   
   
   
   # precision section ---------
   output$precision <- renderText({
-    paste("douzR precision: ", "%") #TODO: getting percision
+    paste("douzR precision: ",sum(!is.na(value_table)) ,"%") #TODO: getting percision
   })
   
   # board section ---------
@@ -37,8 +38,6 @@ server <- function(input, output, session) {
   board_reactive <- reactiveVal(board)
   
   melt_board_reactive <- reactive({
-    
-    
     melt(board_reactive()) %>%
       mutate(Var1 = factor(Var1),
              Var2 = factor(Var2, levels = c(3, 2, 1)))
@@ -75,7 +74,7 @@ server <- function(input, output, session) {
   # player move
   observeEvent(input$player_two_move, {
     # browser()
-    
+    print("player")
     old_state_index(state_index(board))
     
     player_two_move <- player_two_move_reactive()
@@ -88,14 +87,17 @@ server <- function(input, output, session) {
     if (is_done(board)) {
       current_state_index <- state_index(board)
       value_table <<- value_update(old_state_index(), current_state_index, alpha)
-      return(value_get(value_table, current_state_index))
+      winner(value_get(value_table, current_state_index))
+      old_state_index(NULL)
     }
   })
   
   
   # RL move
   observeEvent(old_state_index(), {
+    print("RL")
     req(old_state_index())
+
     player_one_move <- next_move(board, player = player_one, epsilon = eps)
     
     board <<- next_state(board, player = player_one, move = player_one_move$move)
@@ -108,22 +110,10 @@ server <- function(input, output, session) {
     
     if (is_done(board)) {
       index <- state_index(board)
-      return(value_get(value_table, index))
+      winner(value_get(value_table, index))
     }
   })
   
-  
-  # observeEvent(
-  #   input$player_two_move, {
-  #     # browser()
-  #     player_two_move <- player_two_move_reactive()
-  #     while(!(player_two_move %in% possible_moves(board)))
-  #       player_two_move <- player_two_move_reactive()
-  #     
-  #     board <<- next_state(board, player = player_two, move = player_two_move) #player_two(O) move
-  #     board_reactive(board)
-  #   }
-  # )
   
   
   
@@ -138,7 +128,7 @@ server <- function(input, output, session) {
   
   
   output$data <- renderTable({
-    print(player_two_move_reactive())
+    print(winner())
   })
   
   # saving `value_table` ---------
